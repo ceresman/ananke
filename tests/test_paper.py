4f8228d4f2a82f643b796272e3fbc0cb1e82721e
willamhou <1205157517@qq.com>
2024-01-14 11:25:40 +0800

    feat: add mysql and neo4j

--- tests/test_paper.py
+++ tests/test_paper.py
@@ -79,3 +79,143 @@ def get_chunks(docs, start_id = 0, size = 8000, overlap = 256, seps = ["\n\n", "
 
 chunks, sentences = get_chunks([data])
 print(len(get_sentence(chunks[0])))
+
+
+
+from dataclasses import dataclass
+from typing import (
+    TYPE_CHECKING,
+    Any,
+    Callable,
+    ClassVar,
+    Collection,
+    Dict,
+    Iterable,
+    List,
+    Optional,
+    Tuple,
+    Type,
+    TypeVar,
+)
+
+import mysql.connector
+import pymysql
+
+@dataclass
+class Triple:
+    triple_id: long
+    triple_uuid: str
+    subject_name: str
+    subject_desc: str
+    pred_name: str
+    pred_desc: str
+    object_name: str
+    object_desc: str
+
+
+@dataclass
+class Sentence:
+    sent_uuid: str
+    sent_id: long
+    sent_text: str
+    sent_emb_id: long
+    parent_chunk_id: long 
+    parent_doc_id: long
+    triples: List[Triple]
+
+@dataclass
+class Chunk:
+    chunk_uuid: str
+    chunk_id : long
+    chunk_text: str
+    chunk_summary: str
+    chunk_emb_id: long
+    parent_doc_id: long
+    sents: List[Sentence]
+    triples: List[Triple]
+
+@dataclass
+class Meta:
+    meta_uuid: str
+    meta_id: long
+    meta_type: str
+    meta_value: dict
+    meta_json: str
+
+@dataclass
+class Document:
+    doc_uuid: str
+    doc_id : long
+    doc_text: str
+    doc_meta: meta
+    doc_emb_id: long
+
+
+
+class MySQL(object):
+    def __init__(self, **kwargs):
+        self.host = kwargs.get("msyql_host")
+        self.user = kwargs.get("mysql_user")
+        self.passwd = kwargs.get("msyql_password")
+        self.db = kwargs.get("mysql_dbname")
+        self.connect = pymysql.connect(host = self.host, user = self.user, passwd = self.passwd, database = self.db)
+    
+    def insert_document(self, docs: List[Document]):
+
+        return
+
+    def update_document(self, docs: List[Document]):
+
+        return
+
+    def delete_document(self, docs: List[Document]):
+        return
+
+
+    def insert_chunks(self, chunks: List[Chunk]):
+        return
+
+    def update_chunks(self, chunks: List[Chunk]):
+        return
+
+
+    def delete_chunks(self, chunks: List[Chunks]):
+        return
+
+    def insert_sents(self, sents: List[Sentence]):
+        return
+
+    def update_sents(self, sents: List[Sentence]):
+        return
+
+    def delete_sents(self, sents: List[Sentence]):
+        return
+
+    def insert_triples(self, triples: List[Triple]):
+        return
+
+    def update_triples(self, triples: List[Triple]):
+        return
+
+    def delete_triples(self, triples: List[Triple]):
+        return
+
+
+import py2neo
+
+class Graph(object):
+    def __init__(self, **kwargs):
+        self.host = kwargs.get("neo4j_host")
+        self.user = kwargs.get("neo4j_user")
+        self.passwd = kwargs.get("neo4j_passwd")
+        self.graph = py2neo.Graph(host = host, auth = (self.user, self.passwd))
+
+# mydb = mysql.connector.connect(
+#   host="localhost",
+#   user="root",
+#   passwd="123456",
+#   database="runoob_db"
+# )
+# mycursor = mydb.cursor()
+ 
+# mycursor.execute("CREATE TABLE sites (name VARCHAR(255), url VARCHAR(255))")
